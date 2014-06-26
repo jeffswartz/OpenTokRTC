4db41521e552842844124bd73bbd0cc46e642b76
Jeff Swartz <swartz@tokbox.com>
2014-06-26 12:26:22 -0700

    References to session should be global.

--- public/js/room.js
+++ public/js/room.js
@@ -42,16 +42,16 @@ function User(roomId, apiKey, sessionId, token){
   }
   var session;
   function connectToSession() {
-    this.session = OT.initSession( this.apiKey, this.sessionId );
-    this.session.on({
-      "sessionDisconnected": this.sessionDisconnected,
-      "streamCreated": this.streamCreated,
-      "streamDestroyed": this.streamDestroyed,
-      "connectionCreated": this.connectionCreated,
-      "connectionDestroyed": this.connectionDestroyed,
-      "signal": this.signalReceived
+    session = OT.initSession( apiKey, sessionId );
+    session.on({
+      "sessionDisconnected": sessionDisconnected,
+      "streamCreated": streamCreated,
+      "streamDestroyed": streamDestroyed,
+      "connectionCreated": connectionCreated,
+      "connectionDestroyed": connectionDestroyed,
+      "signal": signalReceived
     }, this);
-    this.session.connect(this.token,function(err){
+    session.connect(token,function(err){
       if( err ){
         alert("Unable to connect to session. Sorry");
         return;
@@ -313,7 +313,7 @@ User.prototype.sendSignal = function( type, data, to ){
   if(to){
     signalData.to = to;
   }
-  this.session.signal( signalData, this.errorSignal );
+  session.signal( signalData, this.errorSignal );
 };
 
 User.prototype.setLeaderProperties = function( e ){
