0a0ce0b1e4a9f46dd6ecdda99f9e32d9977e5e1a
Jeff Swartz <swartz@tokbox.com>
2014-06-27 11:13:03 -0700

    Temp change so I can test query string on Safari on Mac OS.

--- public/js/opentok.js
+++ public/js/opentok.js
@@ -4976,11 +4976,14 @@ OTHelpers.centerElement = function(element, width, height) {
     OT.debug('OT.supportsOpenTokExtension()');
 
   //  if (OTHelpers.browserOs() == "iOS" || OTHelpers.browserOs() == "Android") {
+/*
     if (window.navigator.userAgent.toLowerCase().indexOf('iphone') > -1
         || window.navigator.userAgent.toLowerCase().indexOf('ipad') > - 1
         || window.navigator.userAgent.toLowerCase().indexOf('ipod') > - 1
         || window.navigator.userAgent.toLowerCase().indexOf('android') > - 1
       )
+*/
+   if (!OT.checkSystemRequirements())
     {
       return true;
     }
